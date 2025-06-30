498b17bedd2765f727afcffa7b8836695330b9c4
Smil3MoreGH <paul.hart98@googlemail.com>
2025-06-30 19:29:59 +0200

    update vom laptop

--- rest-api-service/src/main/java/de/hochschule/bochum/restapi/RestApiServiceApplication.java
+++ rest-api-service/src/main/java/de/hochschule/bochum/restapi/RestApiServiceApplication.java
@@ -2,8 +2,10 @@ package de.hochschule.bochum.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(scanBasePackages = "de.hochschule.bochum")
+@SpringBootApplication
+@EnableMongoRepositories(basePackages = "de.hochschule.bochum.restapi.repository")
 public class RestApiServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApiServiceApplication.class, args);
